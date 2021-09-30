2c02d5eaa33f75e3bfc951ff1eaa94c5310ae181
Leonardo-Meirelles <leonardomeirelles@terra.com.br>
2021-09-30 17:09:09 -0300

    Challenge finished v1

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+import {GenreResponseProps} from '../App'
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  handleClickButton: any;
+  selectedGenreId: any;
+  genres: GenreResponseProps[];
+}
+
+export function SideBar({handleClickButton, selectedGenreId, genres}: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
